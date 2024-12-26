6f6fa4d4f0222a3a69403b37f5fd4f69771fd86a
PhamKien <phamtrungkienonion@gmail.com>
2024-12-26 13:05:04 +0700

    multithreading

--- src/main/java/java_synchronized/SynchronizedExchangerMain.java
+++ src/main/java/java_synchronized/SynchronizedExchangerMain.java
@@ -8,8 +8,11 @@ public class SynchronizedExchangerMain {
         Thread thread1 = new Thread(() -> {
             synchronized (exchanger) { // Đồng bộ hóa trên đối tượng exchanger
                 for (int i = 0; i < 100; i++) {
-                    exchanger.setObj("" + i);
-                    System.out.println(" " + exchanger.getObj() + "A");
+//                    exchanger.setObj("" + i);
+                    exchanger.object= i;
+
+//                    System.out.println(" " + exchanger.getObj() + "A");
+                    System.out.println(" " + exchanger.object + "A");
                 }
             }
         });
@@ -20,8 +23,12 @@ public class SynchronizedExchangerMain {
                     public void run() {
 //                        synchronized (exchanger) {
                             for (int i = 0; i < 100; i++) {
-                                exchanger.setObj("" + i);
-                                System.out.println(" " + exchanger.getObj() + "B");
+//                                exchanger.setObj("" + i);
+                                exchanger.object= i;
+
+//                                System.out.println(" " + exchanger.getObj() + "B");
+                                System.out.println(" " + exchanger.object + "b");
+
                             }
                         }
 //                    }
@@ -31,4 +38,11 @@ public class SynchronizedExchangerMain {
         thread1.start();
         thread2.start();
     }
+    /*
+    Khối synchronized: Khi bạn có khối mã synchronized, chỉ có một thread tại một thời điểm được phép vào khối đó nếu nó đang sử dụng cùng một đối tượng làm khóa.
+    Trong trường hợp của bạn, thread1 sử dụng synchronized (exchanger), nên nó sẽ chiếm khóa của exchanger.
+
+   Thread không synchronized: Khi bạn comment bỏ khối synchronized trong thread2, nó không còn bị ràng buộc bởi khóa của exchanger nữa.
+   Do đó, cả hai thread có thể chạy song song mà không ghi đè lên nhau. Kết quả là, thread1 và thread2 có thể in ra giá trị đồng thời, và bạn sẽ thấy kết quả không được đồng bộ.
+     */
 }
